90199289946549fbc83a5b0d65e85ee458698814
mshgh <36337240+mshgh@users.noreply.github.com>
2019-07-14 14:55:29 +0000

    Updated message

--- src/HwDemo/Program.cs
+++ src/HwDemo/Program.cs
@@ -6,7 +6,7 @@ namespace HwDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! ---updated---");
         }
     }
 }
