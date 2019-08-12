a7133fac56ec34538972e2c9f4634dc01251df41
mshgh <36337240+mshgh@users.noreply.github.com>
2019-08-12 16:35:08 +0200

    upd

--- src/HwDemo/Program.cs
+++ src/HwDemo/Program.cs
@@ -6,7 +6,7 @@ namespace HwDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! ---update #003---");
+            Console.WriteLine("Hello World! ---update #004---");
         }
     }
 }
